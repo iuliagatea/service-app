3487a261742ec441f2b51ef95e529df752c36684
Iulia Gatea-Malinchi <iuliagatea-malinchi@iulias-mbp.home>
2022-03-17 14:06:45 +0200

    added unit tests for status

---
+++ spec/controllers/statuses_controller_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StatusesController do
+  include_context 'initialize'
+  let!(:statuses) { create_list(:status, 10, tenant_id: tenant.id) }
+  let(:status) { create(:status, tenant_id: tenant.id) }
+  describe 'GET #index' do
+    subject { get :index, tenant_id: tenant.id }
+    let(:tenant_statuses) { tenant.statuses.paginate(page: 1, per_page: 10) }
+    it 'renders a list of statuses' do
+      subject
+      expect(assigns(:statuses)).to match(tenant_statuses)
+    end
+    it { expect(subject).to render_template(:index) }
+  end
+  describe 'GET #new' do
+    subject { get :new, tenant_id: tenant.id }
+    it 'assigns values to variables' do
+      subject
+      expect(assigns(:status).id).to be_nil
+    end
+    it { expect(subject).to render_template(:new) }
+  end
+  describe 'POST #create' do
+    let(:create_attributes) { attributes_for(:status) }
+    subject { post :create, status: create_attributes, tenant_id: tenant.id }
+    it 'saves a new status' do
+      expect { subject }.to change(Status, :count).by(1)
+    end
+  end
+  describe 'GET #show' do
+    subject { get :show, status_id: status.id, tenant_id: tenant.id }
+    before { status }
+    it 'assigns values to variables' do
+      subject
+      expect(assigns(:status)).to eq(status)
+    end
+    it { expect(subject).to render_template(:show) }
+  end
+  describe 'GET #edit' do
+    subject { get :edit, status_id: status.id, tenant_id: tenant.id }
+    before { status }
+    it 'assigns values to variables' do
+      subject
+      expect(assigns(:status)).to eq(status)
+    end
+    it { expect(subject).to render_template(:edit) }
+  end
+  describe 'PATCH #update' do
+    let(:new_status_name) { 'New name' }
+    let(:update_attributes) do
+      { name: new_status_name }
+    end
+    subject { patch :update, status_id: status.id, status: update_attributes, tenant_id: tenant.id }
+    before { status }
+    it 'does not save a new status' do
+      expect { subject }.to change(Status, :count).by(0)
+    end
+    it 'updates status with new params' do
+      subject
+      expect(assigns(:status).name).to eq(new_status_name)
+    end
+  end
+  describe 'Delete #destroy' do
+    subject { delete :destroy, status_id: status.id, tenant_id: tenant.id }
+    before { status }
+    it 'deletes the product' do
+      expect { subject }.to change(Status, :count).by(-1)
+    end
+  end
+end
